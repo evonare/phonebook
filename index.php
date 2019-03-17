bf63eb5608b703eebd667adbb2b526b0aa4ff008
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-17 11:49:44 +0500

    .

--- index.php
+++ index.php
@@ -15,3 +15,15 @@
  */
   
   require('load.php');
+
+?><html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Phonebook</title>
+</head>
+<body>
+  
+</body>
+</html>
