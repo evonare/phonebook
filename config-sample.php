995994afcd042e862850426ae93f22b8f8bf27ed
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-17 11:51:07 +0500

    .

---
+++ config-sample.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright Rana Jahanzaib 2019
+ * 
+ * 
+ * Application Loader
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Phonebook Web App
+ */
+  
+  // Application's database server credentials
+  define("HOST", "");
+  define("DB", "");
+  define("USER", "");
+  define("PASS", "");
+  
+  $link = mysqli_connect(HOST, USER, PASS, DB);
+  
+  if($link) {
+    echo (
+      "<script>console.log('[OK] Database connection');</script>"
+    );
+  }else {
+    echo (
+      "<script>console.log('[FAILED] Database connection');</script>"
+    );
+  }
