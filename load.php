6af098bbd2e0cb4c27bb4d0ab7187283d234063d
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-17 11:24:31 +0500

    .

---
+++ load.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright Rana Jahanzaib 2019
+ * 
+ * 
+ * Application Loader
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Phonebook Web App
+ */
+  
+  // Define ABSPATH as this file's directory
+  if (!defined('ABSPATH'))
+  define('ABSPATH', dirname(__FILE__).'/');
+  require('config.php');
