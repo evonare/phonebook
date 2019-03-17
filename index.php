bd9c8e5a4a08e8fba2e9b58dcb101b83ef449047
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-17 12:00:38 +0500

    .

--- index.php
+++ index.php
@@ -21,9 +21,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href='//cdn.appzaib.com/public/v1/img/appzaib-favico.png' rel='shortcut icon'/>
+	<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Poppins'>
+  <link rel='stylesheet' href='src/css/font-awesome.min.css'/>
+  <link rel='stylesheet' href='src/css/bootstrap.min.css'/>
+  <link rel='stylesheet' href='src/css/app.css'/>
   <title>Phonebook</title>
 </head>
 <body>
-  
+
+  <script src='src/js/jquery.min.js'></script>
+  <script src='src/js/bootstrap.min.js'></script>
+  <script src='src/js/popper.min.js'></script>
 </body>
 </html>
